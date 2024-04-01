916ce75a69c6e9f38ef7d5b73068db67543bb5c8
Marek Watroba <jotthyr@gmail.com>
2024-04-01 11:25:17 +0200

    RC-001 - Blink led PA5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, rusty crab!");
+#![no_std]
+#![no_main]
+
+// Halt when the program panics.
+extern crate panic_halt;
+
+// Includes.
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m_rt::entry;
+use stm32f3::stm32f303;
+
+#[entry]
+fn main() -> ! {
+// Set up SysTick peripheral.
+let cm_p = cortex_m::Peripherals::take().unwrap();
+let mut syst = cm_p.SYST;
+syst.set_clock_source( SystClkSource::Core );
+// ~1s period; STM32L0 boots to a ~2.1MHz internal oscillator.
+syst.set_reload( 2_100_000 );
+syst.enable_counter();
+
+// Set up GPIO pin B3 as push-pull output.
+let p = stm32f303::Peripherals::take().unwrap();
+let gpioa = p.GPIOA;
+unsafe { gpioa.moder.write( |w| w.moder5().bits( 0b01 ) ); }
+gpioa.otyper.write( |w| w.ot5().clear_bit() );
+
+// Restart the SysTick counter.
+syst.clear_current();
+
+// Main loop.
+loop {
+// Toggle the LED every SysTick tick.
+while !syst.has_wrapped() {};
+gpioa.odr.write( |w| w.odr5().set_bit() );
+while !syst.has_wrapped() {};
+gpioa.odr.write( |w| w.odr5().clear_bit() );
+}
 }
